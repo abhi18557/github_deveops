0286cfba42791f4d42760fea2988225cea2b7bd4
Abhishek <231030210@juitsolan.in>
2026-02-05 11:15:45 +0530

    new checkpoints

--- topic1.py
+++ topic1.py
@@ -2,3 +2,5 @@ print("in file system if file is deleted then rollback not possible but in git i
 # Through dir and ls command we get all things when we written the code with topic all details
 #using these two command
 # git status is command for show the status of working directory
+
+#something i am trying to check
