b3284880cb9e7388907d24072dfa9b369bafcb68
Abhishek <231030210@juitsolan.in>
2026-02-05 10:32:40 +0530

    added success

--- topic1.py
+++ topic1.py
@@ -1,1 +1,4 @@
-print("in file system if file is deleted then rollback not possible but in git it is possible")
+print("in file system if file is deleted then rollback not possible but in git is possible")
+# Through dir and ls command we get all things when we written the code with topic all details
+#using these two command
+# git status is command for show the status of working directory
