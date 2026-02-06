45d60db4d4da2ac0cd23f69d15ded1208ee618c2
abhi18557 <abhijuit557@gmail.com>
2026-02-06 14:52:31 +0530

    nice one

--- gitComaands.py
+++ gitComaands.py
@@ -1,8 +1,10 @@
-#2 clear
-  # 3 git status
-  # 4 rm testing.py
- #  5 rm topic1.py
- #  6 git restor topic1.py
-  # 7 git restore topic1.py
-  # 8 clear
-  # 9 git status
+"""
+Git command notes:
+
+git status
+rm testing.py
+rm topic1.py
+git restore topic1.py
+clear
+"""
+#good research is good
