a20a92f9858cdeca19c9d06bcc972d005dbf9741
abhi18557 <abhijuit557@gmail.com>
2026-02-06 15:08:38 +0530

    just checking

--- topic1.py
+++ topic1.py
@@ -4,3 +4,14 @@ print("in file system if file is deleted then rollback not possible but in git i
 # git status is command for show the status of working directory
 
 #something i am trying to check
+   cd ..
+   cd github_deveops
+   git status topic1.py
+   history
+   mkdir my_2project
+   cd my_2project
+   ls
+   git clone https://github.com/abhi18557/Aman-Barnwal-Le... 
+   cd Aman-Barnwal-Leetcode-Solutions
+   ls
+   git history
